c3aa2a84421498172fcb6a8a5d19cbe20ae5f6e6
gbrp <agyava@outlook.com>
2018-12-13 18:24:33 +0100

    if, else, ilif

---
+++ Osztalyozo.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# 5 - 85-100, 4 - 70-84, 3 - 55-69, 2 40-54, 1 0-40
+
+if pont <= 100 and pont  >= 85:
+    print: "5"
+if pont <= 85 and pont  >= 70:
+    print: "4"
+if pont <= 70 and pont  >= 55:
+    print: "3"
+if pont <= 55 and pont  >= 40:
+    print: "2"
+if pont <= 40 and pont  >= 0:
+    print: "1"
+
+if pont >= 85:
+elif pont >= 70:
+    print: "4"
+elif pont >= 55:
+    print: "3"
+elif pont >= 40:
+    print: "2"
+else:
+    print: "1"
